dfb7b3f7c2b7c955247dc1549c9f214a8f503b77
jzaoueli <zaoueli.jihed@gmail.com>
2017-02-12 12:07:57 +0100

    open Email app when click on order button

--- app/src/main/java/com/example/zaoueli/justj/MainActivity.java
+++ app/src/main/java/com/example/zaoueli/justj/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.zaoueli.justj;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -41,6 +43,13 @@ public class MainActivity extends AppCompatActivity {
         String orderSummary = createOrderSummary(name, price, hasWhippedCream, hasChocolate);
         displayMessage(orderSummary);
 
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Order from " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, orderSummary);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     private String createOrderSummary(String name, int price, Boolean hasWhippedCream, Boolean hasChocolate) {
